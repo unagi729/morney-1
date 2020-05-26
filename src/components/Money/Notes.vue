dc2286f947a00ec03daaec96a4d2499f170fe0f4
mandy <535286768@qq.com>
2020-05-27 00:31:56 +0800

    把Money.vue分成四个子组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="在这里输入备注" />
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  background: #f5f5f5;
+  padding-left: 16px;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul class="types">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    line-height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      //使用绝对定位
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
