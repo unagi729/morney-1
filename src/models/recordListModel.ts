0a01c16abfd3f1ba161852e27f34610011f8270a
mandy <535286768@qq.com>
2020-06-01 22:32:41 +0800

    展示和创建标签

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,14 +1,14 @@
 const localStorageKeyName = 'recordList'
-const model = {
+const recordListModel = {
     clone(data: RecordItem[] | RecordItem) {
         return JSON.parse(JSON.stringify(data))
     },
     fetch() {
-        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") as RecordItem;
+        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") as RecordItem[];
     },
     save(data: RecordItem[]) {
         window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
     }
 }
 
-export { model }
+export { recordListModel }
