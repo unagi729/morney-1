7cc865972a71b5aff3337fa8dc897922d67a13e0
mandy <535286768@qq.com>
2020-06-02 21:48:22 +0800

    修复value被赋值的问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,8 +5,7 @@
       <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
       <Types :value.sync="record.type" />
       <div class="notes">
-        <FormItem field-name="备注" placeholder="在这
-      里输入备注" @update:value="onUpdateNotes" />
+        <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes" />
       </div>
       <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
     </Layout>
